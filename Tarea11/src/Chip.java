a2ca826ae1975971267c90d644666a39670b96b7
AlexanderKael <104025778+AlexanderKael@users.noreply.github.com>
2022-06-13 16:35:46 -0500

    Add files via upload

---
+++ Tarea11/src/Chip.java
@@ -0,0 +1,15 @@
+public class Chip {
+    private String empresa;
+    private int numero;
+
+    public Chip(String empresa, int numero) {
+        this.empresa = empresa;
+        this.numero = numero;
+    }
+
+    public void mostrar(){
+        System.out.println("\tEmpresa: "+ empresa);
+        System.out.println("\tNumero de cel: "+ numero);
+    }
+
+}
